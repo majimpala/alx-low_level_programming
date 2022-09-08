4a7032df52d284d7ad15ce11901546aeeea7ff60
majimpala <mrimahjembe19@gmail.com>
2022-09-08 10:32:40 -0700

    alx-low_level_programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,8 @@
  *  * main - prints to string
  *   * Description: Prints "and that piece of art is useful.." without puts
  *    * Return: 1
- *     */
+ *
+ *i/
 
 int main(void)
 {
@@ -13,6 +14,7 @@ int main(void)
 	long fd = 1;
 	long syscall = 1;
 	long ret = 0;
+
 	__asm__ ("syscall"
 			: "=a" (ret)
 			: "a" (syscall),
