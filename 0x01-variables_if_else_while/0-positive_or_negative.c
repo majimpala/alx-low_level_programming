c29e9acacc7499f42f104c503de1a82a780f7877
majimpala <mrimahjembe19@gmail.com>
2022-09-09 04:36:22 -0700

    alx-low_level_programming

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,10 +6,9 @@
  * Description: prints  if a number is positive or negative.
  * Return: 0
  */
- int main(void)
- {
+int main(void)
+{
 	int n;
-	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
